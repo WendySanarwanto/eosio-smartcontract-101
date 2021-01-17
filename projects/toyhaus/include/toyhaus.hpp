8010aa43b4b10ec47134ad204b4211fa25d9dfb9
Wendy Sanarwanto <wendy.sanarwanto@gmail.com>
2021-01-17 18:42:56 +0800

    toyhaus: Added toyshelf multiindex table , upsert & delete actions into the toyhaus SC.

--- projects/toyhaus/include/toyhaus.hpp
+++ projects/toyhaus/include/toyhaus.hpp
@@ -1,10 +1,25 @@
 #include <eosio/eosio.hpp>
 
 using namespace eosio;
+using namespace std;
 
 CONTRACT toyhaus : public contract {
   public:
     using contract::contract;
 
-    ACTION welcome(name arrival);
+    TABLE toy {
+      uint64_t id;
+      string toyname;
+      string toydescrip;
+      uint64_t quantity;
+      name owner = name("toyboss");
+
+      auto primary_key() const { return id; };
+    };
+
+    typedef multi_index<name("toyshelf"), toy> toyshelf_table;
+
+    ACTION addtoy(uint64_t id, string toyname, string toydescrip, uint64_t quantity);
+
+    ACTION removetoy(uint64_t id);
 };
--- projects/toyhaus/src/toyhaus.cpp
+++ projects/toyhaus/src/toyhaus.cpp
@@ -1,8 +1,38 @@
 #include <toyhaus.hpp>
 
-ACTION toyhaus::welcome(name arrival) {
+ACTION toyhaus::addtoy(uint64_t id, string toyname, string toydescrip, uint64_t quantity) {
   // require_auth(name("toyboss"));
-  check( has_auth(arrival), "You are not our owner !" );
-  check( arrival == name("toyboss"), "Please don't lie about your name!" );
-  print ("Welcome to the toyhaus !");
+  check( has_auth(name( "toyboss" )), "You are not our owner !" );
+  toyshelf_table _toyshelf(get_self(), get_self().value);
+
+  // Find toy by specified id
+  auto itr = _toyshelf.find(id); 
+  if (itr == _toyshelf.end()) {
+    print ("Item does not exist. Creating a new item with id:", id);
+    // Item does not exist. Create a new item.
+    _toyshelf.emplace( get_self(), [&](toy& new_toy_row){
+      new_toy_row.id = _toyshelf.available_primary_key();
+      new_toy_row.toyname = toyname;
+      new_toy_row.toydescrip = toydescrip;
+      new_toy_row.quantity = quantity;      
+    }); 
+  } else {
+    print ("Modifying an item with id:", id);
+    _toyshelf.modify(itr, get_self(), [&](toy& row_to_modify){
+      row_to_modify.toyname = toyname;
+      row_to_modify.toydescrip = toydescrip;    
+      row_to_modify.quantity = quantity;
+    });
+  }
 };
+
+ACTION toyhaus::removetoy(uint64_t id) {
+  check( has_auth(name( "toyboss" )), "You are not our owner !" );
+  toyshelf_table _toyshelf(get_self(), get_self().value);
+
+  auto itr = _toyshelf.find(id); 
+  check( itr != _toyshelf.end(), "Toy with given id does not exist. Nothing to delete.");
+
+  print ("Item with specified id does exist. Deleting the item.");
+  _toyshelf.erase(itr);
+}
