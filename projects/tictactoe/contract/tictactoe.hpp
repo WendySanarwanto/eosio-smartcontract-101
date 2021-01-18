f1ba25c95d02f797eef52277dd70599169f3a845
Wendy Sanarwanto <wendy.sanarwanto@gmail.com>
2021-01-18 18:47:43 +0800

    Added secondary index on opponent field

--- projects/tictactoe/contract/tictactoe.hpp
+++ projects/tictactoe/contract/tictactoe.hpp
@@ -10,10 +10,14 @@ CONTRACT tictactoe : public contract {
       name host;
       name opponent;
 
-      auto primary_key() const { return host.value; };
-    };
+      uint64_t primary_key() const { return host.value; };
+      uint64_t by_secondary() const { return opponent.value; };
+      EOSLIB_SERIALIZE(game, (host)(opponent))
+    };  
 
-    typedef multi_index<name("games"), game> games_table;
+    // typedef multi_index<name("games"), game> games_table;
+    typedef multi_index<name("games"), game, 
+                        indexed_by<name("secid"), const_mem_fun<game, uint64_t, &game::by_secondary>>> games_table;    
 
     ACTION welcome( const name &host, const name &opponent );
     ACTION create( const name &challenger, const name &host );
